68e9e393f255a9480bdb06cc07d3e82fd81c4249
kavincode0 <kav.sharma0291@gmail.com>
2023-10-05 23:35:31 +0530

    conditions and loops

---
+++ conditional.js
@@ -0,0 +1,26 @@
+/* Scope, Conditionals
+    const a = this cannot be changed
+    let a = this can be changed
+    if else is the same 
+    switch is the same
+    
+    for loop is the same, 
+    while, do while are same
+
+    => modernized for loops 
+    let fruits = ["Apple", "Kiwi", "Banana"];
+
+    1.fruits.forEach(function f(element)){
+        console.log("fruit"+element)
+    }
+
+    2.elements: for of loop
+    for(element of fruits){
+        console.log("fruit"+element)
+    }
+
+    3.objects: for in loop
+    for(key in emp){
+        console.log('the ${key} of emp is ${emp[key]}')
+    }
+    */
