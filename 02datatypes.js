bb0667dfc31929032b8a36833081ceecce00dd50
kavincode0 <kav.sharma0291@gmail.com>
2023-10-06 21:47:32 +0530

    datatypes

---
+++ 02datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all code as newer JS version
+
+// alert(3+3) we are using nodejs, this syntax works on browser
+// ecma: js standard documentation
+
+// let name = "k"
+// let age = 20
+// let isLoggedIn = false
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean
+// null => stadalone value (empty)
+// undefined
+// symbol => unique
+
+// Object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
