889bc15c5d0629e85a12d598682289b575873451
kavincode0 <kav.sharma0291@gmail.com>
2023-10-07 22:54:50 +0530

    conversionoperation

---
+++ 03coversionoperatioin.js
@@ -0,0 +1,19 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc", undefined => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ''
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//"" => false 
+// "k" => true
