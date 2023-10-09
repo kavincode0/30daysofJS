ebf56a2bf6ac8684f72c909789a557f4901953bd
kavincode0 <kav.sharma0291@gmail.com>
2023-10-09 21:38:35 +0530

    comparision

---
+++ 04comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// not comparing the same datatype
+console.log("2">1)
+console.log("02">1)
+
+// this kind of comparision should be avoided
+console.log(null>0)//false
+console.log(null==0)//false
+console.log(null>=0)//true
+          
+
+// === (strict check, datatype is also checked)
+console.log("2" === 2)
+ 
