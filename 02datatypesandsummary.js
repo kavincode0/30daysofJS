ccbda3e5bbcc77ec2e70af3e348d5dbe5d097fc2
kavincode0 <kav.sharma0291@gmail.com>
2023-10-11 18:16:20 +0530

    stack and heap

--- 02datatypesandsummary.js
+++ 02datatypesandsummary.js
@@ -32,3 +32,26 @@ const myfunc = function(){
 }
 console.log(typeof bigNumber)
 
+// ***************Stack and Heap******************
+// Stack(Primitive)->copy of the value, Heap(Non-Primitive)->reference of the value
+console.log()
+
+let prim1 = "Primitive data is stored in stack"
+let prim2 = prim1
+prim2 = "if stored in new variable, the original variable's value does not change"
+
+console.log(prim1)
+console.log(prim2)
+
+let nprim1 = {
+    val: "Non-primitive data is stored in heap"
+}
+let nprim2 = nprim1
+nprim2.val = "The original data has changed because heap has new variable as reference variable"
+
+console.log()
+console.log(nprim1.val)
+console.log(nprim2.val)
+
+
+
