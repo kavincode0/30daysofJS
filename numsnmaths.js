bb8a3ba20a5df1a064acd59e0ef94713253480ae
kavincode0 <kav.sharma0291@gmail.com>
2023-10-13 00:31:04 +0530

    math object

--- numsnmaths.js
+++ numsnmaths.js
@@ -4,5 +4,20 @@ console.log(typeof bal1)
 bal1 = bal1.toString()
 console.log(typeof bal1)
 console.log(bal1.length)
-console.log(bal1.toFixed(1))//shows the precision value tenth, hunderedth, etc
 
+const num = 123.7439
+console.log(num.toPrecision(2));
+console.log(num.toFixed(1))//shows the precision value tenth, hunderedth, etc
+
+const amt = 10000000
+console.log(amt.toLocaleString('en-IN'))
+
+// *****************Maths********************
+
+console.log(Math);
+console.log(Math.abs(-1));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(3.8));
+console.log(Math.min(23, 39))
+console.log(Math.random());//(0,1)
