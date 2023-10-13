6606fa0f864abbb70eefcacfa277e876a222150f
kavincode0 <kav.sharma0291@gmail.com>
2023-10-14 01:05:48 +0530

    array1

---
+++ array.js
@@ -0,0 +1,33 @@
+// resizable, mixed, prototype access in browser
+const myArr = [3, 4, 5, "k"];
+
+//push, pop
+myArr.push(2)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
+
+// shift, unshift
+myArr.unshift(9)
+console.log(myArr)
+myArr.shift()
+console.log(myArr)
+
+ console.log(myArr.includes(3))
+ console.log(myArr.indexOf(3))
+
+ console.log(typeof myArr);
+ 
+ const newArr = myArr.join();
+ console.log(typeof newArr);
+
+//  slice(manipulates the original array) and splice 
+const smpArr = [1, 2, 3, 4, 5]
+console.log("original: ", smpArr)
+
+const sliceArr = smpArr.slice(1, 3)
+console.log("slice ", sliceArr)
+
+const spliceArr = smpArr.splice(1, 3)
+console.log("splice: ", spliceArr)
+console.log("original to after splice: ", smpArr)
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const bal1 = new Number(100);
-console.log(typeof bal1)
-
-console.log(bal1.toString())
